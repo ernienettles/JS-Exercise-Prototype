bc911462aedf32b2e511b581c302acc00fdbdf9e
Ernie Nettles <ernienettles@gmail.com>
2020-03-25 19:09:31 -0400

    Finished challenges

--- index.js
+++ index.js
@@ -39,8 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+this.name = name,
+this.age = age,
+this.stomach = [];
+}
 
+Person.prototype.toString = function () {
+ return `${this.name}, ${this.age}`;
+}
+
+
+Person.prototype.eat = function(someFood) {
+   if (this.stomach.length < 10) {
+     this.stomach.push(someFood)
+   }
+}
+
+Person.prototype.poop = function() {
+  this.stomach = [];
 }
 
 /*
@@ -57,8 +74,29 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+  
 
+}
+
+Car.prototype.fill = function(gallons) {
+  return this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  let driveableMiles = this.tank * this.milesPerGallon;
+    if (distance < driveableMiles) {
+      let gallonsUsed = driveableMiles - distance;
+      this.tank -= gallonsUsed;
+      this.odometer += distance;
+    } else {
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
 }
 
 /*
@@ -68,18 +106,28 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  this.name = name,
+  this.age = age,
+  this.favoriteToy = favoriteToy;
+  this.name = name;
+  this.age = age;
+}
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. This is global
+  2. This points to the left of the period. aka the object on which the function was called
+  3. You can tell the js engine to set this to point to a certain value using call, apply, or bind.
+  4. Using new constructs a new object that this points to.
 */
 
 
